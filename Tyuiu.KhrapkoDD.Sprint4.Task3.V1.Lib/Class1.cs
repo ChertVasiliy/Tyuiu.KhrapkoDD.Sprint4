0c1a31b3ed7e6437bc89313353b05c94a2cd75eb
ChertVasiliy <kolyaminigan21@gmail.com>
2024-11-12 20:38:24 +0500

    Task3

--- Tyuiu.KhrapkoDD.Sprint4.Task3.V1.Lib/Class1.cs
+++ Tyuiu.KhrapkoDD.Sprint4.Task3.V1.Lib/Class1.cs
@@ -1,7 +1,23 @@
-﻿namespace Tyuiu.KhrapkoDD.Sprint4.Task3.V1.Lib
-{
-    public class Class1
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
+namespace Tyuiu.KhrapkoDD.Sprint4.Task3.V1.Lib
+{
+    public class DataService : ISprint4Task3V1
+    {
+        public int Calculate(int[,] array)
+        {
+            int k = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j == 4)
+                    {
+                        k += array[i, j];
+                    }
+                }
+            }
+            return k;
+        }
     }
 }
