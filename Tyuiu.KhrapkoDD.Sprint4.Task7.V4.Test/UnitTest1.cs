ccf2dd308f90e100e31c936e98ee8dfa0bcf1b7b
ChertVasiliy <kolyaminigan21@gmail.com>
2024-11-12 19:22:00 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KhrapkoDD.Sprint4.Task7.V4.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Tyuiu.KhrapkoDD.Sprint4.Task7.V4.Lib;
+
+namespace Tyuiu.KhrapkoDD.Sprint4.Task7.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(8, ds.Calculate(2, 2, "3456"));
+        }
+    }
+}
